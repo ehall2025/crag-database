aae0ff1d882c620a8efd71fcd6fd0c7b6ec4a2c2
Ethan <halle92602@gmail.com>
2026-05-13 18:11:40 -0500

    created controller test and implemented location controller

---
+++ server/src/main/java/org/cragdatabase/controllers/LocationController.java
@@ -0,0 +1,27 @@
+package org.cragdatabase.controllers;
+
+import org.cragdatabase.domain.LocationService;
+import org.cragdatabase.models.Location;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/location")
+@CrossOrigin
+public class LocationController {
+
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping
+    public List<Location> findAllLocations() {
+        return locationService.findAllLocations();
+    }
+}
