8f76f2161f190be5b81d4afa63b7005a0a2ca563
Ethan <halle92602@gmail.com>
2026-05-14 21:39:49 -0500

    removed unused imports

--- server/src/main/java/org/cragdatabase/domain/UserLoginService.java
+++ server/src/main/java/org/cragdatabase/domain/UserLoginService.java
@@ -2,14 +2,10 @@ package org.cragdatabase.domain;
 
 import org.cragdatabase.data.UserLoginRepository;
 import org.cragdatabase.models.User;
-import org.cragdatabase.models.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
