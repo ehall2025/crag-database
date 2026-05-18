dfa68b63eb8cc3d22f5f1400dc567fb9fffdb6af
Ethan <halle92602@gmail.com>
2026-05-18 10:12:10 -0500

    added findAlls and findByIds for all location tables to repo interface and began implementing in jdbcrepo

--- server/src/main/java/org/cragdatabase/data/LocationJdbcRepository.java
+++ server/src/main/java/org/cragdatabase/data/LocationJdbcRepository.java
@@ -1,7 +1,10 @@
 package org.cragdatabase.data;
 
 import org.cragdatabase.data.mappers.LocationMapper;
+import org.cragdatabase.models.Area;
+import org.cragdatabase.models.Crag;
 import org.cragdatabase.models.Location;
+import org.cragdatabase.models.Route;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +15,11 @@ public class LocationJdbcRepository implements LocationRepository {
 
     private final JdbcClient jdbcClient;
 
-    private final String BASE_SELECT = "SELECT l.id , l.country , l.region , l.description FROM location l";
+    private final String LOCATION_SELECT = "SELECT l.id , l.country , l.region , l.description FROM location l";
+    private final String CRAG_SELECT = "SELECT c.id , c.name , c.location_id , c.description FROM crag c";
+    private final String AREA_SELECT = "SELECT a.id , a.name , a.crag_id , a.super_area_id , a.description FROM area a";
+    private final String ROUTE_SELECT = "SELECT r.id , r.name , r.difficulty , r.description FROM route r";//TODO
+
 
     public LocationJdbcRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
@@ -20,6 +27,45 @@ public class LocationJdbcRepository implements LocationRepository {
 
     @Override
     public List<Location> findAllLocations() {
-        return jdbcClient.sql(BASE_SELECT + ";").query(new LocationMapper()).list();
+        return jdbcClient.sql(LOCATION_SELECT + ";")
+                .query(new LocationMapper())
+                .list();
+    }
+
+    @Override
+    public Location findLocationById(int locationId) {
+        return jdbcClient.sql(LOCATION_SELECT + ";")
+                .query(new LocationMapper())
+                .optional().orElse(null);
+    }
+
+    @Override
+    public List<Crag> findCragsByLocation(int locationId) {
+        return List.of();
+    }
+
+    @Override
+    public Crag findCragById(int cragId) {
+        return null;
+    }
+
+    @Override
+    public List<Area> findAreasByCrag(int cragId) {
+        return List.of();
+    }
+
+    @Override
+    public Crag findAreaById(int areaId) {
+        return null;
+    }
+
+    @Override
+    public List<Route> findRoutesByArea(int areaId) {
+        return List.of();
+    }
+
+    @Override
+    public Crag findRouteById(int routeId) {
+        return null;
     }
 }
