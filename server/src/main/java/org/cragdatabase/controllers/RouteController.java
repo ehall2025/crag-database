901cff29e42c5b473b3a6eaa19f3943cbaec8c6b
Ethan <halle92602@gmail.com>
2026-05-20 11:49:12 -0500

    implemented RouteController methods

--- server/src/main/java/org/cragdatabase/controllers/RouteController.java
+++ server/src/main/java/org/cragdatabase/controllers/RouteController.java
@@ -1,6 +1,10 @@
 package org.cragdatabase.controllers;
 
+import org.cragdatabase.domain.RouteService;
+import org.cragdatabase.domain.results.Result;
+import org.cragdatabase.domain.results.ResultType;
 import org.cragdatabase.models.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +14,60 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class RouteController {
 
+    @Autowired
+    private final RouteService routeService;
+
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
     @PostMapping
     public ResponseEntity userPost (@RequestBody Route route) {
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+        Result<Route> result = routeService.userPost(route);
+
+        if (!result.isSuccess() && result.getResultType() == ResultType.INVALID) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+        } else if (!result.isSuccess()) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PostMapping("/admin")
     public ResponseEntity adminPost (@RequestBody Route route) {
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+        Result<Route> result = routeService.adminPost(route);
+
+        if (!result.isSuccess() && result.getResultType() == ResultType.INVALID) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+        } else if (!result.isSuccess()) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/admin")
     public ResponseEntity adminPut (@RequestBody Route route) {
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+        Result<Route> result = routeService.adminPut(route);
+
+        if (!result.isSuccess() && result.getResultType() == ResultType.INVALID) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+        } else if (!result.isSuccess()) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/{routeId}")
-    public ResponseEntity adminDelete (@RequestBody int routeId) {
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity adminDelete (@PathVariable int routeId) {
+        Result<Route> result = routeService.adminDelete(routeId);
+
+        if (!result.isSuccess()) {
+            return new ResponseEntity(result.getErrorMessages(), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
