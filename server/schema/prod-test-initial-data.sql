b9704102e1f3728118512cc193c5b99fdb937d0c
Ethan <halle92602@gmail.com>
2026-05-20 14:28:43 -0500

    added admin and user account to initial data file

--- server/schema/prod-test-initial-data.sql
+++ server/schema/prod-test-initial-data.sql
@@ -1,5 +1,9 @@
 use crag_database_prod_test_env;
 
+insert into user (email, password, role) values
+	('admin@admin.com', '$2a$12$01igN71/i6pFe7rTDSSGaulCR36jWBrOeAqt7dlFBluw2e5hYugwq', 'ROLE_ADMIN'),
+	('user@user.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER');
+
 insert into location (Country, Region, description) values
 	('United States', 'Wisconsin', 'A heavily forested landscape nesled in the glacial valley which gave way to the unique types of rock formations and compositions such as baraboo quartzite which is only found in the near the small town of baraboo'),
 	('United States', 'Minnesota', 'Despite not being well know for its climbing, Minnesota has some of the greatest diversity of rock types in the country. Minnesota also has deep roots in climbings history being home to one of, if not the, first commercial climbing gym in the country and many big names accross of parts of the climbing industry hailing from this land of 10,000 lakes');
