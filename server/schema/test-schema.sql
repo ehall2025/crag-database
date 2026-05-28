76eb9494ed96538350bc15192c693eb7f0dfd5dd
Ethan <halle92602@gmail.com>
2026-05-28 11:13:04 -0500

    updated tables in test-schema to match prod schema and added test data for geographic layers to setKnownGoodState()

--- server/schema/test-schema.sql
+++ server/schema/test-schema.sql
@@ -129,10 +129,6 @@ begin
     delete from location;
     alter table location auto_increment = 1;
 
-    insert into location (Country, Region, description) values
-	('United States', 'Wisconsin', 'A heavily forested landscape nesled in the glacial valley which gave way to the unique types of rock formations and compositions such as baraboo quartzite which is only found in the near the small town of baraboo'),
-	('United States', 'Minnesota', 'Despite not being well know for its climbing, Minnesota has some of the greatest diversity of rock types in the country. Minnesota also has deep roots in climbings history being home to one of, if not the, first commercial climbing gym in the country and many big names accross of parts of the climbing industry hailing from this land of 10,000 lakes');
-
     insert into user (email, password, role) values
     ('jim_merli@rockmaster.com', '$2a$12$01igN71/i6pFe7rTDSSGaulCR36jWBrOeAqt7dlFBluw2e5hYugwq', 'ROLE_ADMIN'),
     ('tommy@4fingers.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER'),
