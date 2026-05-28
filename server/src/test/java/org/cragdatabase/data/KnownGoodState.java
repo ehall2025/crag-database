35e88c5c9b1afee94eef284f29634a1567998444
Matthew Ma <MatthewMa929@Gmail.com>
2026-05-28 10:27:05 -0400

    userlogin service test

--- server/src/test/java/org/cragdatabase/data/KnownGoodState.java
+++ server/src/test/java/org/cragdatabase/data/KnownGoodState.java
@@ -1,6 +1,7 @@
 package org.cragdatabase.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,12 @@ public class KnownGoodState {
     @Autowired
     JdbcClient jdbcClient;
 
-    public void set() {
-        jdbcClient.sql("call set_known_good_state();").update();
+    static boolean hasRun = false;
+
+    void set() {
+        if (!hasRun) {
+            hasRun = true;
+            jdbcClient.sql("call set_known_good_state();").update();
+        }
     }
 }
