2c43b069c523acc1e2e99d038ea795ff3d8b32af
Ethan <halle92602@gmail.com>
2026-05-28 11:50:38 -0500

    fixed set_known_good_state() dataInegrityExpception and create test for getStagedRoutes

--- server/src/test/java/org/cragdatabase/data/RouteJdbcRepositoryTest.java
+++ server/src/test/java/org/cragdatabase/data/RouteJdbcRepositoryTest.java
@@ -1,9 +1,13 @@
 package org.cragdatabase.data;
 
+import org.cragdatabase.models.Route;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,9 +20,18 @@ class RouteJdbcRepositoryTest {
     @Autowired
     private JdbcClient jdbcClient;
 
+    @BeforeEach
+    void setup() {
+        jdbcClient.sql("call set_known_good_state();").update();
+    }
+
     @Test
     void shouldFindStagedRoutes() {
+        List<Route> actual = repository.getStagedRoutes();
 
+        assertEquals(2, actual.size());
+        assertEquals("Panic Room", actual.get(0).getName());
+        assertEquals("Ghostly Grips", actual.get(1).getName());
     }
 
     @Test
@@ -28,6 +41,36 @@ class RouteJdbcRepositoryTest {
 
     @Test
     void shouldPostToRoute() {
+
+    }
+
+    @Test
+    void shouldUpdateRoute() {
+
+    }
+
+    @Test
+    void shouldNotUpdateRoute() {
+
+    }
+
+    @Test
+    void shouldDeleteRoute() {
+
+    }
+
+    @Test
+    void shouldNotDeleteRoute() {
+
+    }
+
+    @Test
+    void shouldDeleteStagedRoute() {
+
+    }
+
+    @Test
+    void shouldNotDeleteStagedRoute() {
 
     }
 }
