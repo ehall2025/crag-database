cbc7ccee7c43b06bb8d2a859ba33ee5cff8e6fcb
Ethan <halle92602@gmail.com>
2026-05-28 13:47:52 -0500

    created test for RouteService.getStagedRoutes

--- server/src/test/java/org/cragdatabase/domain/RouteServiceTest.java
+++ server/src/test/java/org/cragdatabase/domain/RouteServiceTest.java
@@ -1,7 +1,117 @@
 package org.cragdatabase.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.cragdatabase.data.RouteRepository;
+import org.cragdatabase.domain.results.Result;
+import org.cragdatabase.models.Route;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RouteServiceTest {
+
+    @Autowired
+    RouteService service;
+
+    @MockBean
+    RouteRepository repository;
+
+    @Test
+    void shouldFindStagedRoutes() {
+        when(repository.getStagedRoutes()).thenReturn(List.of());
+        Result<List<Route>> actual = service.getStagedRoutes();
+        assertEquals(0, actual.getpayload().size());
+    }
+
+    @Test
+    void shouldPostStagedRoute() {
+
+    }
+
+    @Test
+    void shouldNotPostStagedRouteNoName() {
+
+    }
+
+    @Test
+    void shouldNotPostStagedRouteNoDescription() {
+
+    }
+
+    @Test
+    void shouldNotPostStagedRouteNoStartPosition() {
+
+    }
+
+    @Test
+    void shouldNotPostStagedRouteBadArea() {
+
+    }
+
+    @Test
+    void shouldPostRoute() {
+
+    }
+
+    @Test
+    void shouldNotPostRouteNoName() {
+
+    }
+
+    @Test
+    void shouldNotPostRouteNoDescription() {
+
+    }
+
+    @Test
+    void shouldNotPostRouteNoStartPosition() {
+
+    }
+
+    @Test
+    void shouldNotPostRouteBadArea() {
+
+    }
+
+    @Test
+    void shouldUpdateRoute() {
+
+    }
+
+    @Test
+    void shouldNotUpdateRouteNoName() {
+
+    }
+
+    @Test
+    void shouldNotUpdateRouteNoDescription() {
+
+    }
+
+    @Test
+    void shouldNotUpdateRouteNoStartPosition() {
+
+    }
+
+    @Test
+    void shouldNotUpdateRouteBadArea() {
+
+    }
+
+    @Test
+    void shouldDelete() {
+
+    }
+
+    @Test
+    void shouldNotDelete() {
+
+    }
 
 }
