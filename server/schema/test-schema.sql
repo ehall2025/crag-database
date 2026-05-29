84823baef36491ca265295312f96bee4b40e01b3
Matthew Ma <MatthewMa929@Gmail.com>
2026-05-29 13:31:54 -0400

    plaintext password comments on known good state

--- server/schema/test-schema.sql
+++ server/schema/test-schema.sql
@@ -139,10 +139,10 @@ begin
     alter table user auto_increment = 1;
 
     -- Insert in dependency order (parent before child)
-    insert into user (email, password, role) values
-    ('jim_merli@rockmaster.com', '$2a$12$01igN71/i6pFe7rTDSSGaulCR36jWBrOeAqt7dlFBluw2e5hYugwq', 'ROLE_ADMIN'),
-    ('tommy@4fingers.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER'),
-    ('user2@user.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER');
+    insert into user (email, password, role) values --Comments are the password in plaintext
+    ('jim_merli@rockmaster.com', '$2a$12$01igN71/i6pFe7rTDSSGaulCR36jWBrOeAqt7dlFBluw2e5hYugwq', 'ROLE_ADMIN'), --admin
+    ('tommy@4fingers.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER'), --user
+    ('user2@user.com', '$2a$12$S2Uu.lenW46InFyq6.dJ2ubt1Xsdwe8iVfXSCzRkA1V6tZCccU6.G', 'ROLE_USER'); --user
 
     insert into location (Country, Region, description) values
         ('United States', 'Wisconsin', ''),
